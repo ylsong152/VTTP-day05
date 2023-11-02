6e00dcc6e85ec4da42cbcc92caa90744fd1b9f58
Yong Le <ylsong152@gmail.com>
2023-11-02 11:56:53 +0800

    Chuk's solution

---
+++ src/sdf/day05/Book.java
@@ -0,0 +1,28 @@
+package sdf.day05;
+
+public class Book {
+    private String title;
+    private String publisher;
+
+    public Book (String title, String publisher) {
+        this.title = title;
+        this.publisher = publisher;
+    }
+
+    public String getBookTitle() {
+        return title;
+    }
+
+    public void setBookTitle(String bookTitle) {
+        this.title = bookTitle;
+    }
+
+    public String getBookPublisher() {
+        return publisher.toUpperCase();
+    }
+
+    public void setBookPublisher(String bookPublisher) {
+        this.publisher = bookPublisher;
+    }
+
+}
--- src/sdf/day05/BookPublisher.java
+++ src/sdf/day05/BookPublisher.java
@@ -1,14 +1,14 @@
 package sdf.day05;
 
 import java.io.*;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-
 public class BookPublisher {
-
+    public static int COL_TITLE = 1;
+    public static int COL_PUBLISHER = 11;
     public static void main(String[] args) {
-
 
         if(args.length <= 0) {
             System.err.println("Missing book CSV");
@@ -18,25 +18,37 @@ public class BookPublisher {
 
         try(BufferedReader br = new BufferedReader(new FileReader(args[0]))) {
 
-            Map<String, Long> publishCount = br.lines()
+            Map<String, List<Book>> classified = br.lines()
             .skip(1)
-            .map(line -> line.split(","))
-            .map(arr -> arr[arr.length - 1])
-            .collect(Collectors.groupingBy(publisher -> publisher.trim().toUpperCase(), Collectors.counting()));
+            .map(row -> row.trim().split(","))
+            .map(fields -> new Book(fields[COL_TITLE], fields[COL_PUBLISHER]))
+            .collect(Collectors.groupingBy(book -> book.getBookPublisher()));
 
-            // publishCount.forEach((publisher, count) -> System.out.println(publisher + ": " + count));
-            publishCount.forEach((publisher, count) -> System.out.printf("%s: %d\n", publisher, count));
+            for (String publisher : classified.keySet()) {
+                List<Book> books = classified.get(publisher);
+                System.out.printf("%s (%d)\n", publisher, books.size());
+                for (Book book : books) {
+                    System.out.printf("\t%s\n", book.getBookTitle());
+                }
+            }
+
+
+            // .map(arr -> arr[arr.length - 1])
+            // .collect(Collectors.groupingBy(publisher -> publisher.trim().toUpperCase(), Collectors.counting()));
+            // classified.forEach((publisher, count) -> System.out.printf("%s: %d\n", publisher, count));    
             
+
+            // List convert Map
             // List<String[]> a = br.lines()
             // .skip(1)
             // .map(line -> line.trim().split(","))
             // .collect(Collectors.toList());
 
-            // Map<String, Long> publishCount = a.stream()
+            // Map<String, Long> classified = a.stream()
             // .map(arr -> arr[11])
             // .collect(Collectors.groupingBy(publisher -> publisher, Collectors.counting()));
             
-            // publishCount.forEach((publisher, count) -> System.out.println(publisher + ": " + count));
+            // classified.forEach((publisher, count) -> System.out.println(publisher + ": " + count));
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
